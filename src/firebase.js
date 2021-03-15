a6a53a152237420a36988e4395b36415c86336dc
abdulahifarid <61620889+abdulahifarid@users.noreply.github.com>
2021-03-15 09:05:59 +0300

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAsH7er5uPWa1v09g0Sk6oVAAo3L9cpEvY",
+  authDomain: "twitter-clone-ac97f.firebaseapp.com",
+  projectId: "twitter-clone-ac97f",
+  storageBucket: "twitter-clone-ac97f.appspot.com",
+  messagingSenderId: "183111907668",
+  appId: "1:183111907668:web:535c4f3ac4b1c51d1e70c1",
+};
