d12cba25a4427f7dc0cd02d60cd7397c1f43fbc7
abdulahifarid <61620889+abdulahifarid@users.noreply.github.com>
2021-03-15 10:04:43 +0300

    completed sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Twitter Clone</h1>
+      <Sidebar />
     </div>
   );
 }
